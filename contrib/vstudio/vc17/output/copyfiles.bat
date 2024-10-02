84e84193ed88c85aedd05eea5c61b581e7c53b8c
WB\wenbo <1045031242@qq.com>
2024-10-02 10:28:26 +0800

    拷贝脚本优化

--- contrib/vstudio/vc17/output/copyfiles.bat
+++ contrib/vstudio/vc17/output/copyfiles.bat
@@ -1,47 +1,60 @@
 
-rem x86 release build
+rem 1.copy files to x86 release
  rem Copy the .h files to the include directory
- if not exist ".\include" mkdir ".\include"
-copy /Y "..\..\..\..\zlib.h" ".\include\zlib.h"
-copy /Y "..\..\..\..\zconf.h" ".\include\zconf.h"
+ if not exist ".\include" mkdir ".\x86\Release\include"
+copy /Y "..\..\..\..\zlib.h" ".\x86\Release\include\zlib.h"
+copy /Y "..\..\..\..\zconf.h" ".\x86\Release\include\zconf.h"
 
  rem  Copy the zlib dll to the bin directory
- if not exist ".\bin\x86\Release" mkdir ".\bin\x86\Release"
-copy /Y "..\x86\ZlibDll\Release\zlibwapi.dll" ".\bin\x86\Release\zlibwapi.dll"
+ if not exist ".\bin\x86\Release" mkdir ".\x86\Release\bin"
+copy /Y "..\x86\ZlibDll\Release\zlibwapi.dll" ".\x86\Release\bin\zlib.dll"
 
  rem  Copy the zlib lib to the bin directory
-if not exist ".\lib\x86\Release" mkdir ".\lib\x86\Release"
-copy /Y "..\x86\ZlibDll\Release\zlibwapi.lib" ".\lib\x86\Release\zlibwapi.lib"
-copy /Y "..\x86\ZlibDll\Release\zlibwapi.pdb" ".\lib\x86\Release\zlibwapi.pdb"
+if not exist ".\lib\x86\Release" mkdir ".\x86\Release\lib"
+copy /Y "..\x86\ZlibDll\Release\zlibwapi.lib" ".\x86\Release\lib\zlib.lib"
+copy /Y "..\x86\ZlibDll\Release\zlibwapi.pdb" ".\x86\Release\lib\zlib.pdb"
 
 
-rem x86 debug build
+rem 2.copy files to x86 debug
+rem Copy the .h files to the include directory
+ if not exist ".\include" mkdir ".\x86\Debug\include"
+copy /Y "..\..\..\..\zlib.h" ".\x86\Debug\include\zlib.h"
+copy /Y "..\..\..\..\zconf.h" ".\x86\Debug\include\zconf.h"
+
  rem  Copy the zlib dll to the bin directory
- if not exist ".\bin\x86\Debug" mkdir ".\bin\x86\Debug"
-copy /Y "..\x86\ZlibDll\Debug\zlibwapi.dll" ".\bin\x86\Debug\zlibwapi.dll"
+ if not exist ".\bin\x86\Debug" mkdir ".\x86\Debug\bin\"
+copy /Y "..\x86\ZlibDll\Debug\zlibwapi.dll" ".\x86\Debug\bin\zlib.dll"
 
  rem  Copy the zlib lib to the bin directory
-if not exist ".\lib\x86\Debug" mkdir ".\lib\x86\Debug"
-copy /Y "..\x86\ZlibDll\Debug\zlibwapi.lib" ".\lib\x86\Debug\zlibwapi.lib"
-copy /Y "..\x86\ZlibDll\Debug\zlibwapi.pdb" ".\lib\x86\Debug\zlibwapi.pdb"
+if not exist ".\lib\x86\Debug" mkdir ".\x86\Debug\lib"
+copy /Y "..\x86\ZlibDll\Debug\zlibwapi.lib" ".\x86\Debug\lib\zlib.lib"
+copy /Y "..\x86\ZlibDll\Debug\zlibwapi.pdb" ".\x86\Debug\lib\zlib.pdb"
 
 
-rem x64 release build
+rem 3. copy files to x64 release
+if not exist ".\include" mkdir ".\x64\Release\include"
+copy /Y "..\..\..\..\zlib.h" ".\x64\Release\include\zlib.h"
+copy /Y "..\..\..\..\zconf.h" ".\x64\Release\include\zconf.h"
+
  rem  Copy the zlib dll to the bin directory
- if not exist ".\bin\x64\Release" mkdir ".\bin\x64\Release"
-copy /Y "..\x64\ZlibDll\Release\zlibwapi.dll" ".\bin\x64\Release\zlibwapi.dll"
+ if not exist ".\bin\x64\Release" mkdir ".\x64\Release\bin"
+copy /Y "..\x64\ZlibDll\Release\zlibwapi.dll" ".\x64\Release\bin\zlib.dll"
 
  rem  Copy the zlib lib to the bin directory
-if not exist ".\lib\x64\Release" mkdir ".\lib\x64\Release"
-copy /Y "..\x64\ZlibDll\Release\zlibwapi.lib" ".\lib\x64\Release\zlibwapi.lib"
-copy /Y "..\x64\ZlibDll\Release\zlibwapi.pdb" ".\lib\x64\Release\zlibwapi.pdb"
+if not exist ".\lib\x64\Release" mkdir ".\x64\Release\lib"
+copy /Y "..\x64\ZlibDll\Release\zlibwapi.lib" ".\x64\Release\lib\zlib.lib"
+copy /Y "..\x64\ZlibDll\Release\zlibwapi.pdb" ".\x64\Release\lib\zlib.pdb"
 
-rem x64 debug build
+rem 4.copy files to  x64 debug
+if not exist ".\include" mkdir ".\x64\Debug\include"
+copy /Y "..\..\..\..\zlib.h" ".\x64\Debug\include\zlib.h"
+copy /Y "..\..\..\..\zconf.h" ".\x64\Debug\include\zconf.h"
+
  rem  Copy the zlib dll to the bin directory
- if not exist ".\bin\x64\debug" mkdir ".\bin\x64\debug"
-copy /Y "..\x64\ZlibDll\debug\zlibwapi.dll" ".\bin\x64\debug\zlibwapi.dll"
+ if not exist ".\bin\x64\debug" mkdir ".\x64\Debug\bin"
+copy /Y "..\x64\ZlibDll\debug\zlibwapi.dll" ".\x64\Debug\bin\zlib.dll"
 
  rem  Copy the zlib lib to the bin directory
-if not exist ".\lib\x64\debug" mkdir ".\lib\x64\debug"
-copy /Y "..\x64\ZlibDll\debug\zlibwapi.lib" ".\lib\x64\debug\zlibwapi.lib"
-copy /Y "..\x64\ZlibDll\debug\zlibwapi.pdb" ".\lib\x64\debug\zlibwapi.pdb"
+if not exist ".\lib\x64\debug" mkdir ".\x64\Debug\lib"
+copy /Y "..\x64\ZlibDll\debug\zlibwapi.lib" ".\x64\Debug\lib\zlib.lib"
+copy /Y "..\x64\ZlibDll\debug\zlibwapi.pdb" ".\x64\Debug\lib\zlib.pdb"
